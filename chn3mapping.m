d4d0acf19cea181f54382a61cf13725887a4348c
frblanco <feig@bang-olufsen.dk>
2023-04-14 10:35:15 +0200

    This we fixed the logic on the 3 channel function and change the way we access the step array for 12 channels

--- chn3mapping.m
+++ chn3mapping.m
@@ -1,6 +1,6 @@
 function [array] = chn3mapping(value1, value2, value3, step)
 % This miracle function will make that no two channels have the same value, and
-% that the ordering of the values will follow [Left Right Center] 
+% that the ordering of the values will follow [Left Center Right].
 %
 % This function works considering a wrapping of 180. Having at clockwise from
 % 0 to -180.
@@ -9,24 +9,25 @@ function [array] = chn3mapping(value1, value2, value3, step)
 array = sort([value1 value2 value3], 'descend'); 
 % array(1)= Left, array(2)=center, array(3)=right
 
-chn = [1 3]; %Random Channels to be altered. Either Left (1), Center (2) or Right (3)
+chn = [1 3]; %Random Channels to be altered. Either Left (1) or Right (3)
 opr = [1 -1];% Random Operator to be applied, to follow the direction of the channel
-if abs(array(1) - array(2)) < step
+
+if abs(array(1) - array(3)) <= step
 rdm_idx = randi([1 2]);
-array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); % Increasing the distance between Left (1) and Right (2) channel.
+array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); % Increasing the distance between Left (1) from the Right (3) channel.
 end
 
-if abs(array(1) - array(3)) < step
-rdm_idx = randi([1 1]);
-array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); % Increasing the distance between Left (1) and Right (2) channel.
+if abs(array(1) - array(2)) < step
+rdm_idx = 1;
+array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); % Increasing the distance either of the Left (1) or Center (2) channel.
 end
 
 if abs(array(2) - array(3)) < step
-rdm_idx = randi([2 2]);
-array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); % Increasing the distance between Left (1) and Right (2) channel.
+rdm_idx = 2;
+array(chn(rdm_idx)) = array(chn(rdm_idx)) + 2*step*opr(rdm_idx); %Increasing the distance between Right (3) from the Center (2) channel.
 end 
 
 
 
-array = sort(array,'descend')
+array = sort(array,'descend');
 end
--- randompos.m
+++ randompos.m
@@ -19,7 +19,7 @@ for o=1:length(reference)
         arrayname{k,1} = {[num2str(channels(1,1)) 'chn']};
         placement_array(k,1) = {reference(o).placement;};
     end
-    k=k+1
+    k=k+1;
 end
 
 i = 1;
@@ -42,24 +42,49 @@ for i=1:5:size(reference,2)*5;
         otherwise
             msg = 'elevation not available in AVIL lab';
             error(msg)
-    end           
+    end
     
     %% version 1 = reference
+    
     version1 = cell2mat(placement_array(i,1)); % Just Replicated reference. Used later for other versions, but no need to overwrite placement_array(1)
     i=i+1; %increment to next version
     
     %% version 1_lw = low anchor
+  
     % maitain valuen from  placement_array(2:5:12)
     i=i+1; %increment to next version
     
     %% version 2 = angular distortion
-    version2 = cell2mat(placement_array(i,1));
     
+    version2 = cell2mat(placement_array(i,1));    
     % loop over all channels (j) of current config
     for j=1:channels
-        
+        % check for different step for different
+        %step for different elevation angle. Do this inside the j loop,
+         %because in 12 channel there is different elevation with different
+         %step size
+         switch channels
+             case 2
+                  stepidx = 1;
+             case 5
+                  stepidx = 1;
+             case 12
+                 switch version2(j,3)
+                     %Init() parameters
+                     case 0
+                         stepidx = 1;
+                     case abs(28)
+                         stepidx = 2;
+                     case abs(56)
+                         stepidx = 3;
+                     case abs(80)
+                         stepidx = 4;                         
+                     otherwise
+                          stepidx = 1;
+                 end
+         end
         % 1. define an array of possible speaker positions
-        arrayAzimuths = ((version1(j,2)-channel_zone+step):step:(version1(j,2)+channel_zone-step));
+        arrayAzimuths = ((version1(j,2)-channel_zone+step(stepidx)):step(stepidx):(version1(j,2)+channel_zone-step(stepidx)));
         
         % 2. select a random element from array
         idx = randi([1,length(arrayAzimuths)]);
@@ -75,49 +100,31 @@ for i=1:5:size(reference,2)*5;
             % Chs 1 and 2 must not be equal
             % Chs 1 must be greater than Chn 2
             tmp = sort(version2(:,2), 'descend');
-            version2(:,2) = tmp; % sorted in descending order, just assign (1) to Left and (2) to Right   
+            version2(:,2) = tmp; % sorted in descending order, just assign (1) to Left and (2) to Right
             
         case 5
-          
-            % 
             % Chs 1,2 and 3 must not be equal
             % Ch 3 must be less than Ch 1 && greater than Ch 2
             % Delta Ch 1-2 must at least the step size
-            % Delta Ch 1-3 must at least the step size          
+            % Delta Ch 1-3 must at least the step size
             % Delta Ch 2-3 must at least the step size
             [tmp] = chn3mapping(version2(1,2) , version2(2,2), version2(3,2), step);
+            version2(1:3,2) = tmp([1 3 2]);
             
-           
-            version2(1:3,2) = tmp([1 3 2]); 
-                  
             % Ch 4 must be greater than Chn 5
             tmp = sort(version2(4:5,2), 'descend');
             version2(4:5,2) = tmp; % sorted in descending order, just assign (4) to Left Surround and (5) to Right Surround
-        
-        case 12
-            %Init() parameters
-           switch version2(j,3)
-                case 0
-                    step = step(1);
-                case abs(28)
-                    step = step(2);
-                case abs(56)
-                     step = step(3);
-                case abs(80)
-                     step = step(4);
-               otherwise
-                   step =15;
-            end
             
+        case 12
             % Chs 1,2 and 3 must not be equal
             % Ch 3 must be less than Ch 1 && greater than Ch 2
             % Delta Ch 1-2 must at least the step size
-            % Delta Ch 1-3 must at least the step size          
+            % Delta Ch 1-3 must at least the step size
             % Delta Ch 2-3 must at least the step size
-          
-            [tmp] = chn3mapping(version2(1,2), version2(2,2), version2(3,2), step);
-            version2(1:3,2) = tmp([1 3 2]); 
-                  
+            
+            [tmp] = chn3mapping(version2(1,2), version2(2,2), version2(3,2), step(stepidx));
+            version2(1:3,2) = tmp([1 3 2]);
+            
             % Ch 4 must be greater than Chn 5
             tmp = sort(version2(5:6,2), 'descend');
             version2(5:6,2) = tmp; % sorted in descending order, just assign (5) to Left Surround Rear and (6) to Right Surround Rear
@@ -125,32 +132,34 @@ for i=1:5:size(reference,2)*5;
             % Ch 6 must be greater than Chn 7
             tmp = sort(version2(7:8,2), 'descend');
             version2(7:8,2) = tmp; % sorted in descending order, just assign (7) to Left Surround Side and (8) to Right Surround Side
-           
+            
             % Ch 8 must be greater than Chn 9
             tmp = sort(version2(9:10,2), 'descend');
             version2(9:10,2) = tmp; % sorted in descending order, just assign (9) to Upper Left and (10) to Upper Right
-           
-            % Ch 10 must be greater than Chn 11            
+            
+            % Ch 10 must be greater than Chn 11
             tmp = sort(version2(11:12,2), 'descend');
-            version2(11:12,2) = tmp; % sorted in descending order, just assign (11) to Upper Left Surround Rear and 
-                                     %(12) to Upper Right Surround Near
+            version2(11:12,2) = tmp; % sorted in descending order, just assign (11) to Upper Left Surround Rear and (12) to Upper Right Surround Near
         otherwise
             msg = 'Channel format may not be supported.';
             error(msg)
-    end          
-     
+    end
+    
     placement_array{i,1}= version2;
     i=i+1;
     
-    %% version 3 = distance distortion 
-   
-    version3 =cell2mat(placement_array(i,1)); 
+    %% version 3 = distance distortion
+    
+    version3 =cell2mat(placement_array(i,1));
     distance = [0:0.1:maxdistance]; %Define array of possible distances.
     for j=1:1:size(version3,1)
-        idx = randi([1,length(distance)]); %Random Index to select one of the possible distances from "distance"
-        version3(j,4) = distance(  idx  ); %Overrite the reference value with the new one. 
-        distance(idx) = []; %Delete the value used above so there is no same value for the distances
-    end         
+        %1a. Random Index to select one of the possible distances from "distance"
+        idx = randi([1,length(distance)]);
+        %1b. Overrite the reference value with the new one.
+        version3(j,4) = distance(  idx  );
+        %1c. Delete the value used above so there is no same value for the distances
+        distance(idx) = []; 
+    end
     placement_array{i,1}= version3;
     i=i+1;
     
@@ -158,13 +167,37 @@ for i=1:5:size(reference,2)*5;
     
     version4 =(cell2mat(placement_array(i,1)));
     distance = [0:0.1:maxdistance];
-       
-       % loop over all channels (j) of current config
+    
+    % loop over all channels (j) of current config
     for j=1:channels
+         % check for different step for different
+         %step for different elevation angle. Do this inside the j loop,
+         %because in 12 channel there is different elevation with different
+         %step size
+      switch channels
+             case 2
+                  stepidx = 1;
+             case 5
+                  stepidx = 1;
+             case 12
+                 switch version4(j,3)
+                     %Init() parameters
+                     case 0
+                         stepidx = 1;
+                     case abs(28)
+                         stepidx = 2;
+                     case abs(56)
+                         stepidx = 3;
+                     case abs(80)
+                         stepidx = 4;                         
+                     otherwise
+                          stepidx = 1;
+                 end
+         end
         %1.  Angle Distortion
         
         %1a. define an array of possible speaker positions
-        arrayAzimuths = ((version1(j,2)-channel_zone+step):step:(version1(j,2)+channel_zone-step));
+        arrayAzimuths = ((version1(j,2)-channel_zone+step(stepidx)):step(stepidx):(version1(j,2)+channel_zone-step(stepidx)));
         %1b. select a random element from array
         idx = randi([1,length(arrayAzimuths)]);
         %1c. assign speaker position to output array
@@ -174,63 +207,43 @@ for i=1:5:size(reference,2)*5;
         
         %2a. Random distance value
         idx = randi([1,length(distance)]); %Select a random Index to select value from "distance"
-        %2b. Overwrite the reference value with the new one. 
-        version4(j,4) = distance(  idx  ); 
+        %2b. Overwrite the reference value with the new one.
+        version4(j,4) = distance(  idx  );
         %2c. Delete the value used above so there is no same value for the distances
-        distance(idx) = []; 
+        distance(idx) = [];
         
     end
     
-     % sanity checks
+    % sanity checks
     switch channels
         case 2
             % Chs 1 and 2 must not be equal
             % Chs 1 must be greater than Chn 2
             tmp = sort(version4(:,2), 'descend');
-            version4(:,2) = tmp; % sorted in descending order, just assign (1) to Left and (2) to Right   
+            version4(:,2) = tmp; % sorted in descending order, just assign (1) to Left and (2) to Right
             
         case 5
-          
-            % 
             % Chs 1,2 and 3 must not be equal
             % Ch 3 must be less than Ch 1 && greater than Ch 2
             % Delta Ch 1-2 must at least the step size
-            % Delta Ch 1-3 must at least the step size          
+            % Delta Ch 1-3 must at least the step size
             % Delta Ch 2-3 must at least the step size
-            [tmp] = chn3mapping(version4(1,2) , version4(2,2), version4(3,2), step);
+            [tmp] = chn3mapping(version4(1,2) , version4(2,2), version4(3,2), step(stepidx));
+            version4(1:3,2) = tmp([1 3 2]);
             
-           
-            version4(1:3,2) = tmp([1 3 2]); 
-                  
             % Ch 4 must be greater than Chn 5
             tmp = sort(version4(4:5,2), 'descend');
             version4(4:5,2) = tmp; % sorted in descending order, just assign (4) to Left Surround and (5) to Right Surround
-        
-        case 12
-            %Init() parameters
-           step = [15 30 60 90]; % [el0, el28, el56, el80]
-           switch version4(j,3)
-                case 0
-                    step = step(1);
-                case abs(28)
-                    step = step(2);
-                case abs(56)
-                     step = step(3);
-                case abs(80)
-                     step = step(4);
-               otherwise
-                   step =15;
-            end
             
+        case 12                  
             % Chs 1,2 and 3 must not be equal
             % Ch 3 must be less than Ch 1 && greater than Ch 2
             % Delta Ch 1-2 must at least the step size
-            % Delta Ch 1-3 must at least the step size          
+            % Delta Ch 1-3 must at least the step size
             % Delta Ch 2-3 must at least the step size
-          
-            [tmp] = chn3mapping(version4(1,2), version4(2,2), version4(3,2), step);
-            version4(1:3,2) = tmp([1 3 2]); 
-                  
+            [tmp] = chn3mapping(version4(1,2), version4(2,2), version4(3,2), step(stepidx));
+            version4(1:3,2) = tmp([1 3 2]);
+            
             % Ch 4 must be greater than Chn 5
             tmp = sort(version4(5:6,2), 'descend');
             version4(5:6,2) = tmp; % sorted in descending order, just assign (5) to Left Surround Rear and (6) to Right Surround Rear
@@ -238,23 +251,19 @@ for i=1:5:size(reference,2)*5;
             % Ch 6 must be greater than Chn 7
             tmp = sort(version2(7:8,2), 'descend');
             version4(7:8,2) = tmp; % sorted in descending order, just assign (7) to Left Surround Side and (8) to Right Surround Side
-           
+            
             % Ch 8 must be greater than Chn 9
             tmp = sort(version4(9:10,2), 'descend');
             version4(9:10,2) = tmp; % sorted in descending order, just assign (9) to Upper Left and (10) to Upper Right
-           
-            % Ch 10 must be greater than Chn 11            
+            
+            % Ch 10 must be greater than Chn 11
             tmp = sort(version4(11:12,2), 'descend');
-            version4(11:12,2) = tmp; % sorted in descending order, just assign (11) to Upper Left Surround Rear and 
-                                     %(12) to Upper Right Surround Near
+            version4(11:12,2) = tmp; % sorted in descending order, just assign (11) to Upper Left Surround Rear and
+            %(12) to Upper Right Surround Near
         otherwise
             msg = 'Channel format may not be supported.';
             error(msg)
-    end          
-     
-        
-    
-   
+    end
     placement_array{i,1}= version4;
     % NO INCREMENT NEEDED; END OF LOOP!
 end
